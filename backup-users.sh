845d10139121380cccbd8f7af78d5ad75d407d26
Konate <setiguikonate@gmail.com>
2026-01-27 20:21:01 +0100

    feat: ajout du nettoyage automatique des anciennes sauvegardes

--- backup-users.sh
+++ backup-users.sh
@@ -13,4 +13,8 @@ mkdir -p $BACKUP_DIR
 echo "Sauvegarde en cours..."
 # tar -czf $BACKUP_DIR/users_$DATE.tar.gz /home/
 
+# Nettoyage des sauvegardes de plus de 7 jours
+echo "Nettoyage des anciennes sauvegardes..."
+# find $BACKUP_DIR -name "users_*.tar.gz" -mtime +7 -delete
+
 echo "Sauvegarde terminée : $BACKUP_DIR/users_$DATE.tar.gz"
